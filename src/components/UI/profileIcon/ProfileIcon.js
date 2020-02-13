d54f79897f90732adf529b4411cbdb40d8f37fc7
David Fioravanti <gainstrive@gmail.com>
2020-02-12 23:48:31 -0500

    Added a profile icon frame.

---
+++ src/components/UI/profileIcon/ProfileIcon.js
@@ -0,0 +1,86 @@
+import React from "react";
+// LINK TO CUSTOM CSS FILE FOR THIS COMPONENT
+import "./ProfileIcon.css";
+
+const ProfileIcon = () => {
+
+    return (
+      <div id="profileIconDiv" className="profileIconDiv">
+            <pre className="profileIcon">
+                {String.raw`
+                                    ':/osyhhdhhhyso+/:.'                                              
+                               -smMMMMMMMMMMMMMMMMMMMMmdyo/-'                                       
+                             :dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNdy+-                                   
+                            oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMms-                                
+                           +MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd+'                             
+                        :shNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNo'                           
+                      'hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm/                          
+                     'dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh.                        
+                     +MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm-                       
+                     mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm.                      
+                    :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy                      
+                    yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.                     
+                   'NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo                     
+                   /MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy                     
+                   yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd                     
+                   dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd                     
+                   mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh                     
+                   hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs                     
+                   oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM/                     
+                   .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.                     
+                    yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd                      
+                    -NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo'                     
+                   .yNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNy'                    
+                   oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM:                    
+                   oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN'                    
+                   /MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+                     
+                   'NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo                      
+                    yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs                       
+                    .NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM-                       
+                     :NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh                        
+                      .yNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy'                        
+                        ./hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNho-                          
+                          :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs                             
+                          'MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+                             
+                           yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM-                             
+                           -MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm                              
+                            oNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM/                              
+                             oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNo                               
+                             +MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd                                
+                             /MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs                                
+                             /MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+                                
+                             :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM/                                
+                             -MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM/                                
+                             .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+                                
+                             'MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs                                
+                             :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN-                               
+                           :dNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNy/.                            
+                           mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmh/'                         
+                           oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN-                         
+                            /mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN:                          
+                             .sNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN:                           
+                               -yNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN:                            
+                                 -yNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN:                             
+                                   -yNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN:                              
+                                     -yNMMMMMMMMMMMMMMMMMMMMMMMMMMMN/                               
+                                       -yNMMMMMMMMMMMMMMMMMMMMMMMMN/                                
+                                         -yNMMMMMMMMMMMMMMMMMMMMMN/                                 
+                                           :hNMMMMMMMMMMMMMMMMMMN/                                  
+                                             :dMMMMMMMMMMMMMMMMN/                                   
+                                              '+mMMMMMMMMMMMMMN/                                    
+                                                .sNMMMMMMMMMMN/                                     
+                                                  -dMMMMMMMMN/                                      
+                                                   'oNMMMMMN/                                       
+                                                     -mMMMN/                                        
+                                                      'dMN/                                         
+                                                       'd/                                          
+                                                        '                                           
+
+                `}
+            </pre>
+        </div>
+    )
+}
+
+// CHANGE OBJECT TO NAME OF COMPONENT TO EXPORT
+export default ProfileIcon;
