96f3f4689d727fadec435bd526043a6011ee24c2
David Fioravanti <gainstrive@gmail.com>
2020-02-14 18:24:56 -0500

    Updated routing according to index.js

--- src/components/pages/game/Game.js
+++ src/components/pages/game/Game.js
@@ -15,7 +15,7 @@ class Game extends Component {
         return (
             <div id="gameWrapper" className="text-center">
                 <Navigation/>
-                <iframe title="Game Screen" id="gameScreen" src="/start"></iframe>
+                <iframe title="Game Screen" id="gameScreen" src="/game/screens/start"></iframe>
             </div>
         )
     }
