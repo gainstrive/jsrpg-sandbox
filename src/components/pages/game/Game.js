20b08b32bf9b766415aaade87d0262f0127a3032
David Fioravanti <gainstrive@gmail.com>
2020-02-14 22:13:39 -0500

    Added devConsole button.

--- src/components/pages/game/Game.js
+++ src/components/pages/game/Game.js
@@ -16,6 +16,7 @@ class Game extends Component {
             <div id="gameWrapper" className="text-center">
                 <Navigation/>
                 <iframe title="Game Screen" id="gameScreen" src="/game/screens/start"></iframe>
+                <button id="devConsoleButton" className="btn btn-success" type="button">&#x26A0;</button>
             </div>
         )
     }
