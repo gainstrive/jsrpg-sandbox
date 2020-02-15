9ec2d7697f59ad6b53416c5f8f5c6c1a75156b1c
David Fioravanti <gainstrive@gmail.com>
2020-02-14 22:12:35 -0500

    Added handler for back button.

--- src/components/pages/game/screens/start/Start.js
+++ src/components/pages/game/screens/start/Start.js
@@ -9,8 +9,8 @@ import "./Start.css";
 
 const game = {
     config: {
-        musicVolume: .1,
-        sfxVolume: .1,
+        musicVolume: .2,
+        sfxVolume: .2,
     },
     playSound: {
         buttonHover: () => {
@@ -26,6 +26,14 @@ class Start extends Component {
         startS3DisplayMessage: "",
         displayName: "GUEST",
     };
+    componentDidMount() {
+        document.getElementById("profileIconDiv").addEventListener("mouseover", () => {
+            this.handleMouseOver("profileIcon", "button", "popupShow", "startS3DisplayMessageDiv");
+        });
+        document.getElementById("profileIconDiv").addEventListener("mouseleave", () => {
+            this.handleMouseLeave("profileIcon", "button", "popupHide", "startS3DisplayMessageDiv");
+        });
+    }
     handleMouseOver = (elem, type, action, target) => {
         if (type === "button") {
             game.playSound.buttonHover();
@@ -134,13 +142,17 @@ class Start extends Component {
             }
         }, 1500);
     };
-    componentDidMount() {
-        document.getElementById("profileIconDiv").addEventListener("mouseover", () => {
-            this.handleMouseOver("profileIcon", "button", "popupShow", "startS3DisplayMessageDiv");
-        });
-        document.getElementById("profileIconDiv").addEventListener("mouseleave", () => {
-            this.handleMouseLeave("profileIcon", "button", "popupHide", "startS3DisplayMessageDiv");
-        });
+    handleBackButton = (currentScreen) => {
+        if (currentScreen === "startS3Wrapper") {
+            let elem1 = document.getElementById(currentScreen);
+            elem1.classList.remove("fadeIn");
+            elem1.classList.add("fadeOut");
+            setTimeout(() => {
+                window.location.reload();
+            }, 1500);
+        }
+    }
+    handleKeyUp = (event) => {
     }
     render() {
 
@@ -154,7 +166,7 @@ class Start extends Component {
                         <h2 id="startS1SubHeader">A ROUGELITE DUNGEON CRAWLER</h2>
                     </div>
                     <div id="startS1EnemyDiv">
-                        <Hellbat />
+                        <Hellbat static="true" />
                     </div>
                     <div id="startS1FormDiv" className="container text-center">
                         <div className="row">
@@ -162,14 +174,19 @@ class Start extends Component {
                                 <p id="startS1FormMessage">LOGIN TO JSRPG:</p>
                             </div>
                             <div className="col">
-                                <button id="startS1FormHelp" className="btn btn-success" type="button">?</button>
+                                <button id="startS1FormHelp" className="btn btn-success" type="button"
+                                onMouseOver={() => this.handleMouseOver("startS1FormHelp", "button", "n/a", "n/a")}
+                                >?</button>
                             </div>
                         </div>
-                        <input id="startS1Email" placeholder="ENTER YOUR EMAIL" className="form-control" type="text" name="" />
+                        <input id="startS1Email" placeholder="ENTER YOUR EMAIL" className="form-control" type="text" name="" 
+                        onKeyUp={(e) => this.handleKeyUp(document.onkeydown = (e))}/>
                         <input id="startS1Password" placeholder="ENTER YOUR PASSWORD" className="form-control" type="text" name="" />
                         <div className="row">
                             <div className="col">
-                                <button id="startS1Login" className="btn btn-success" type="button">LOGIN</button>
+                                <button id="startS1Login" className="btn btn-success" type="button"
+                                onMouseOver={() => this.handleMouseOver("startS1Login", "button", "n/a", "n/a")}
+                                >LOGIN</button>
                             </div>
                             <div className="col">
                                 <input id="startS1PasswordToggle" className="form-check-input" type="checkbox" name="" value="true" />
@@ -177,12 +194,15 @@ class Start extends Component {
                             </div>
                         </div>
                         <div className="col">
-                            <button id="startS1SignUp" className="btn btn-success" type="button">SIGN-UP NOW FOR FREE!</button>
+                            <button id="startS1SignUp" className="btn btn-success" type="button"
+                            onMouseOver={() => this.handleMouseOver("startS1SignUp", "button", "n/a", "n/a")}
+                            >SIGN-UP NOW FOR FREE!</button>
                         </div>
                     </div>
                     <div>
                         <h2 id="startS1GuestText">- OR -</h2>
                         <button id="startS1Guest" onClick={() => this.handleScreenFadeOut("startS1Wrapper", "startS3Wrapper")}
+                            onMouseOver={() => this.handleMouseOver("startS1Guest", "button", "n/a", "n/a")}
                             className="btn btn-success" type="button">PLAY AS GUEST (LOCAL SAVES ONLY)</button>
                     </div>
                 </div>
@@ -215,6 +235,7 @@ class Start extends Component {
                             <div className="col float-left">
                                 <button id="startS3Back" onMouseOver={() => this.handleMouseOver("back", "button", "popupShow","startS3DisplayMessageDiv")} 
                                 onMouseLeave={() => this.handleMouseLeave("back", "button", "popupHide","startS3DisplayMessageDiv")} 
+                                onClick={() => this.handleBackButton("startS3Wrapper")}
                                 className="btn btn-success" type="button">BACK</button>
                             </div>
                             <div className="col float-right">
