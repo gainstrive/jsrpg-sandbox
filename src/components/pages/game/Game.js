88c925a7c675aea8315fd06f3279b17a53f00d71
David Fioravanti <gainstrive@gmail.com>
2020-02-18 20:51:56 -0500

    Added styling trigger for menu toggle.

--- src/components/pages/game/Game.js
+++ src/components/pages/game/Game.js
@@ -4,9 +4,19 @@ import Navigation from '../../elements/navigation/Navigation';
 
 class Game extends Component {
     state = {
-
+        menuToggled: false
     }
     componentDidMount() {
+        document.getElementById("toggler").addEventListener("click", () => {
+            if (!document.getElementById("navbarNav").classList.contains("show") && this.state.menuToggled === false) {
+                document.getElementById("gameScreen").style.top = "240px";
+                this.setState({ menuToggled: true });
+            }
+            else {
+                document.getElementById("gameScreen").style.top = "60px";
+                this.setState({ menuToggled: false });
+            }
+        });
         document.addEventListener("keyup", (e) => {
             if (e.which === 192) {
                 this.testPage("/game/screens/createChar")
